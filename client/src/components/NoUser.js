beb66bb9ba7b6ce9d3010fb26d5337c8eb6fb982
Ankit Kr Chowdhury <89239354+ani1609@users.noreply.github.com>
2023-11-29 15:38:34 +0530

    responsiveness tweaks

--- client/src/components/NoUser.js
+++ client/src/components/NoUser.js
@@ -5,7 +5,7 @@ import Default from "../images/default.png";
 
 function NoUser(props) {
     return (
-        <div className="noUser_container flex flex-col gap-4">
+        <div className="noUser_container">
             <img src={Default} alt="No User" />
             <button onClick={()=>props.setShowSignupForm(true)} className="bg-black border-2 transition-all duration-500 text-white rounded-lg p-2 px-40 hover:bg-white hover:text-black ease-in-out">Get Started</button>
         </div>
