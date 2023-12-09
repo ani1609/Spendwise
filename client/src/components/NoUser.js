8cddc89e316b7fb9db3cf944cd56169a44e109aa
SahilNalavade <sahilnalavade25@gmail.com>
2023-12-10 01:23:43 +0530

    fixed: 'Get Started' button remains in a single

--- client/src/components/NoUser.js
+++ client/src/components/NoUser.js
@@ -11,7 +11,7 @@ function NoUser(props) {
                 onClick={() => props.setShowSignupForm(true)}
                 className="bg-black border-2 transition-all duration-500 text-white rounded-lg p-2 px-8 md:px-40 hover:bg-white hover:text-black ease-in-out text-sm md:text-base lg:text-lg max-w-full"
             >
-            Get Started
+            Get Started 
             </button>
         </div>
     );
