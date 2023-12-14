242aee3cc2ce98aa5bb9f7e25d4958b9320145fc
harshikagoyal14 <109340203+harshikagoyal14@users.noreply.github.com>
2023-12-14 14:09:24 +0530

    Sidebar added for Profile Section (#206)
    * sidebar added
    
    * Update App.js
    
    App.js file openig and enclosing tags bug fix
    
    * Update App.js
    
    App.js bug fix
    
    * minor bug fixes and making separate functions for different pages
    
    ---------
    
    Co-authored-by: Ankit Kr Chowdhury <89239354+ani1609@users.noreply.github.com>

---
+++ client/src/components/profile/RightDashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const RightDashboard = () => {
+    return (
+        <div className="profile-container">
+            
+           
+
+        </div>
+    );
+}
+
+export default RightDashboard;
