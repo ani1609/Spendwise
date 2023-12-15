99476c7a76bda12b25273a79920da6a9a52f60e9
Gautam Raj <gautamraj996@gmail.com>
2023-12-15 17:02:10 +0530

    overlapping problem solved

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import Sidebar from './Sidebar'; 
-import RightDashboard from './RightDashboard'; 
+import React,{useState} from 'react';
+import Sidebar from './Sidebar';
+import RightDashboard from './RightDashboard';
 import PredefinedTransactions from './PredefinedTransactions';
 
 const Profile = () => {
-    return (
-        <div className="profile-container flex">
-            <Sidebar/>
-            {/* <RightDashboard/> */}
-            <PredefinedTransactions/>
-        </div>
-    );
-}
+  const [content, setContent] = useState('profile');
+
+  const handleButtonClick = (selectedContent) => {
+    setContent(selectedContent);
+  };
+  return (
+    <div className='profile-container flex'>
+      <Sidebar onButtonClick={handleButtonClick} />
+      {content === 'profile' && <RightDashboard />}
+      {content === 'predefinedTransactions' && <PredefinedTransactions />}
+    </div>
+  );
+};
 
 export default Profile;
