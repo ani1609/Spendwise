f26fde0589beab181585456f000f494dea468b11
Gautam Raj <gautamraj996@gmail.com>
2023-12-15 13:44:13 +0530

    hamburger made for profile page

--- client/src/components/profile/PredefinedTransactions.js
+++ client/src/components/profile/PredefinedTransactions.js
@@ -7,15 +7,15 @@ const PredefinedTransactions = () => {
 
   return (
     <>
-      <div className='relative left-60 pl-2' style={{width:dynamicWidth}}>
+      <div className='relative sm:left-60 -left-0 -z-10 sm:w-[calc(100%-240px)] w-[100%] pl-2'>
         <div className='font-[Inter] '>
-          <h1 className='font-bold text-[#024164] text-xl mb-6'>
+          <h1 className='font-bold text-[#024164] sm:text-xl text-base mb-6 sm:text-left text-center'>
             Predefined Transactions
           </h1>
           <h2 className='font-medium text-[19px] text-[#024164] mb-2 text-center sm:text-left'>
             Income/Expense
           </h2>
-          <div className='flex flex-wrap gap-5 justify-start'>
+          <div className='flex flex-wrap gap-5 sm:justify-start justify-center'>
             <div className='w-60 h-64 border-2 border-[#9EBEFA] flex flex-col justify-center items-center'>
               <img src={plusFill} alt='' />
               <button className='text-sm font-semibold text-white bg-[#9EBEFA] w-[167px] h-[35px] rounded-xl'>
--- client/src/components/profile/Sidebar.js
+++ client/src/components/profile/Sidebar.js
@@ -1,28 +1,56 @@
-import React from 'react';
-import "../../styles/App.css";
-import { FaUser, FaListAlt, FaSignOutAlt } from 'react-icons/fa';
+import React, { useState,useEffect } from 'react';
+import '../../styles/App.css';
+import { FaUser, FaListAlt, FaSignOutAlt,FaTimes, FaBars } from 'react-icons/fa';
 
 const Sidebar = () => {
-  const sidebarClasses = `sidebar fixed left-0 top-0 h-screen w-60 text-black transition-width`;
+  // const sidebarClasses = `sidebar fixed left-0 top-0 h-screen w-60 text-black transition-width`;
+  const [isSidebarVisible, setSidebarVisible] = useState(false);
 
+  const toggleSidebar = () => {
+    setSidebarVisible(!isSidebarVisible);
+  };
+
+  const sidebarClasses = `sidebar fixed left-0 top-0 h-screen w-60 text-black transform transition-transform ${
+    isSidebarVisible ? 'translate-x-0 transition-transform duration-300 ease-in' : '-translate-x-full transition-transform duration-300 ease-out'
+  }`;
+  useEffect(() => {
+    // Check screen width and set initial sidebar visibility
+    const handleResize = () => {
+      const screenWidth = window.innerWidth;
+      setSidebarVisible(screenWidth >= 640); // Adjust the breakpoint as needed
+    };
+
+    handleResize(); // Set initial state
+
+    // Listen for window resize events
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
   return (
-    <div className={sidebarClasses} style={{ backgroundColor: '#EAF0FB' }}>
-      
-      <div className="buttons p-4 mt-36  ">
-        <button className="flex items-center mb-8 ">
-          <FaUser className="mr-2 " />
-          Profile
-        </button>
-        <button className="flex items-center mb-8">
-          <FaListAlt className="mr-2" />
-          Predefined Transactions
-        </button>
-        <button className="flex items-center mb-8">
-          <FaSignOutAlt className="mr-2" />
-          Logout
-        </button>
+    <>
+      <FaBars onClick={toggleSidebar} className='mx-4 my-7 text-3xl absolute cursor-pointer sm:hidden block'/>
+      <div className={sidebarClasses} style={{ backgroundColor: '#EAF0FB' }}>
+        <div className='buttons p-4 mt-36  '>
+          <FaTimes className='absolute top-4 right-4 text-3xl cursor-pointer sm:hidden block' onClick={toggleSidebar}/>
+          <button className='flex items-center mb-8 '>
+            <FaUser className='mr-2 ' />
+            Profile
+          </button>
+          <button className='flex items-center mb-8'>
+            <FaListAlt className='mr-2' />
+            Predefined Transactions
+          </button>
+          <button className='flex items-center mb-8'>
+            <FaSignOutAlt className='mr-2' />
+            Logout
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
