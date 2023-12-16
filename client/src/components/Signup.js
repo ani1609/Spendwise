cab63ed774bd804957731f05c52be338f81e502c
Ankit Kr Chowdhury <89239354+ani1609@users.noreply.github.com>
2023-12-17 01:06:23 +0530

    login signup forms ui tweak

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -95,14 +95,15 @@ function Signup()
                 />
                 {passwordUnmatched && <p>Passwords do not match</p>}
                 {userExists && <p>User already exists</p>}
-                <button type='submit' style={{ width: '100%', cursor: loading ? 'not-allowed' : 'pointer' }} disabled={loading}>
+                <button type='submit' style={{ marginTop: "10px", width: '100%', cursor: loading ? 'not-allowed' : 'pointer' }} disabled={loading} className="signupBtn">
                     {loading ? (
                         <div className="loading-spinner"></div>
                     ) : (
                         'Sign up'
                     )}
                 </button>
-                <button className="p-2 border flex justify-center gap-2 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150" style={{ width: '100%', backgroundColor:"white", color:"black", animationDelay:"1.6s"}}>
+                <div className="signupSeparator flex justify-center items-center" style={{width: "100%"}}><hr style={{width: "100%"}}></hr> &nbsp;&nbsp;or&nbsp;&nbsp; <hr style={{width: "100%"}}></hr></div>
+                <button className="googleSignup p-2 border flex justify-center gap-2 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150" style={{ backgroundColor:"white", color:"black", animationDelay:"1.7s"}}>
                     <img className="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo" />
                     <span>Continue with Google</span>
                 </button>
