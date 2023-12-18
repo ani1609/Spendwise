51725a678411d38f6ca14a1dc02522ec97eb2a5d
mendacium <sakshamlinkedin@gmail.com>
2023-12-19 00:12:10 +0530

    configured eslintrc.json

--- server/configDB/mongoDB.js
+++ server/configDB/mongoDB.js
@@ -2,15 +2,11 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const { MONGO_URI } = process.env;
 
-const connectDb = async () => 
-{
-    try 
-    {
+const connectDb = async () => {
+    try {
         await mongoose.connect(MONGO_URI);
         console.log('Connected to MongoDB');
-    }
-    catch (error)
-    {
+    } catch (error) {
         console.error('Error connecting to MongoDB:', error);
     }
 };
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,38 +4,29 @@ const bcrypt = require('bcrypt');
 require('dotenv').config();
 const { SECRET_KEY, SALT } = process.env;
 
-const login=async(req,res)=>
-{
-    try
-    {
-        const {email,password}=req.body;
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
         const user = await User.findOne({ email });
-        if (!user)
-        {
-            return res.status(401).send({message:"invalid email or password"});
+        if (!user) {
+            return res.status(401).send({ message: 'invalid email or password' });
         }
-        const isMatch=await bcrypt.compare(password,user.password);
-        if (!isMatch)
-        {
-            return res.status(401).send({message:"invalid email or password"});
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(401).send({ message: 'invalid email or password' });
         }
         const token = jwt.sign({ id: user._id }, SECRET_KEY, { expiresIn: '7d' });
         res.status(200).json({ token });
-    }
-    catch(error)
-    {
-        return res.status(500).send({message:"Internal Server Error"});
+    } catch (error) {
+        return res.status(500).send({ message: 'Internal Server Error' });
     }
 };
 
-const signup = async (req, res) => 
-{
-    try
-    {
+const signup = async (req, res) => {
+    try {
         const user = await User.findOne({ email: req.body.email });
-        if (user) 
-        {
-            return res.status(409).send({ message: "User already exists" });
+        if (user) {
+            return res.status(409).send({ message: 'User already exists' });
         }
 
         const salt = await bcrypt.genSalt(Number(SALT));
@@ -43,39 +34,30 @@ const signup = async (req, res) =>
         const newUser = await new User({ ...req.body, password: hashedPassword }).save();
 
         const token = jwt.sign({ id: newUser._id }, SECRET_KEY, { expiresIn: '7d' });
-        res.status(201).send({user:newUser, token: token});
-
-    }
-    catch (error) 
-    {
-        return res.status(500).send({ message: "Internal Server Error" });
+        res.status(201).send({ user: newUser, token });
+    } catch (error) {
+        return res.status(500).send({ message: 'Internal Server Error' });
     }
 };
 
-function authenticateJWT(req, res, next) 
-{
+function authenticateJWT (req, res, next) {
     const authorizationHeader = req.header('Authorization');
-    if (!authorizationHeader) 
-    {
+    if (!authorizationHeader) {
         return res.status(401).json({ message: 'Authentication failed: No token provided.' });
     }
 
     const token = authorizationHeader.split(' ')[1];
 
-    jwt.verify(token, SECRET_KEY, (err, decoded) => 
-    {
-        if (err) 
-        {
-        return res.status(403).json({ message: 'Authentication failed: Invalid token.' });
+    jwt.verify(token, SECRET_KEY, (err, decoded) => {
+        if (err) {
+            return res.status(403).json({ message: 'Authentication failed: Invalid token.' });
         }
         User.findOne({ _id: decoded.id })
-            .then(user => 
-            {
+            .then(user => {
                 req.user = user;
                 next();
             })
-            .catch(error => 
-                {
+            .catch(error => {
                 return res.status(500).json({ message: 'Internal Server Error' });
             });
     });
@@ -88,11 +70,9 @@ const userProfile = (req, res) => {
     res.json({ message: 'User profile accessed successfully!', user });
 };
 
-
-
 module.exports = {
     login,
     signup,
     userProfile,
-    authenticateJWT
+    authenticateJWT,
 };
