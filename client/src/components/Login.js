915302c18a871f83567a030751132c9e0c90eb6a
mendacium <sakshamlinkedin@gmail.com>
2023-12-22 23:58:00 +0530

    made password show toggle

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,7 +3,6 @@ import "../index.css";
 import "../styles/Login.css";
 import axios from "axios";
 import { ReactComponent as Close } from "../icons/close.svg";
-
 // import search from "../icons/search.svg";
 
 function Login ({ setShowLoginForm }) {
@@ -13,6 +12,7 @@ function Login ({ setShowLoginForm }) {
     password: ""
   });
   const [loading, setLoading] = useState(false);
+  const [passwordShow, setpasswordShow] = useState(true);
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -39,41 +39,47 @@ function Login ({ setShowLoginForm }) {
   };
 
   return (
-        <div className="login_form_container" onClick={(e) => e.stopPropagation()}>
-            <h1>Welcome Back</h1>
-            <div className='close-icon' onClick={ () => { setShowLoginForm(false); } }>
-                <Close fill="white" className='w-6 h-6 cursor-pointer'/>
-            </div>
-            <form onSubmit={handleLogin}>
-                <input
-                    type='email'
-                    placeholder='Email'
-                    value={loginData.email}
-                    onChange={(e) => setLoginData({ ...loginData, email: e.target.value })}
-                    required
-                />
-                <input
-                    type='password'
-                    placeholder='Password'
-                    value={loginData.password}
-                    onChange={(e) => setLoginData({ ...loginData, password: e.target.value })}
-                    required
-                />
-                {invalidEmail && <p className="error_message">Invalid email or password</p>}
-
-                <button type='submit' style={{ marginTop: "10px", width: "100%", cursor: loading ? "not-allowed" : "pointer" }} disabled={loading} className='loginBtn'>
-                    {loading
-                      ? (<div className="loading-spinner"></div>)
-                      : ("Log in" // Note: 'Log in' should be a string
-                        )}
-                </button>
-                <div className="loginSeparator flex justify-center items-center" style={{ width: "100%" }}><hr style={{ width: "100%" }}></hr> &nbsp;&nbsp;or&nbsp;&nbsp; <hr style={{ width: "100%" }}></hr></div>
-                <button className="googleLogin p-2 border flex justify-center gap-2 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150" style={{ width: "100%", backgroundColor: "white", color: "black", animationDelay: "1.3s" }}>
-                    <img className="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo" />
-                    <span>Continue with Google</span>
-                </button>
-            </form>
+    <div className="login_form_container" onClick={(e) => e.stopPropagation()}>
+      <h1>Welcome Back</h1>
+      <div className='close-icon' onClick={() => { setShowLoginForm(false); }}>
+        <Close fill="white" className='w-6 h-6 cursor-pointer' />
+      </div>
+      <form onSubmit={handleLogin}>
+        <input
+          type='email'
+          placeholder='Email'
+          value={loginData.email}
+          onChange={(e) => setLoginData({ ...loginData, email: e.target.value })}
+          required
+        />
+        <div className="flex flex-nowrap justify-between w-full">
+        <input
+          type={passwordShow ? "password" : "text"}
+          placeholder='Password'
+          value={loginData.password}
+          onChange={(e) => setLoginData({ ...loginData, password: e.target.value })}
+          required
+        />
+        <div onClick={ () => setpasswordShow(!passwordShow)}>
+        {passwordShow
+          ? <img className="w-10 h-10" src="https://www.svgrepo.com/show/532320/lock.svg" alt="" />
+          : <img className="w-10 h-10" src="https://www.svgrepo.com/show/532324/lock-slash.svg" alt="" />}</div>
         </div>
+        {invalidEmail && <p className="error_message">Invalid email or password</p>}
+
+        <button type='submit' style={{ marginTop: "10px", width: "100%", cursor: loading ? "not-allowed" : "pointer" }} disabled={loading} className='loginBtn'>
+          {loading
+            ? (<div className="loading-spinner"></div>)
+            : ("Log in" // Note: 'Log in' should be a string
+              )}
+        </button>
+        <div className="loginSeparator flex justify-center items-center" style={{ width: "100%" }}><hr style={{ width: "100%" }}></hr> &nbsp;&nbsp;or&nbsp;&nbsp; <hr style={{ width: "100%" }}></hr></div>
+        <button className="googleLogin p-2 border flex justify-center gap-2 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150" style={{ width: "100%", backgroundColor: "white", color: "black", animationDelay: "1.3s" }}>
+          <img className="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo" />
+          <span>Continue with Google</span>
+        </button>
+      </form>password-svgrepo-com
+    </div>
   );
 }
 
