f7c26f5cbb1b53b06213801420c3abe1f4a98608
Ankit Kr Chowdhury <89239354+ani1609@users.noreply.github.com>
2023-12-24 13:06:03 +0530

    code optimisation

--- server/models/user.js
+++ server/models/user.js
@@ -23,13 +23,4 @@ const userSchema = new mongoose.Schema
 
 const User = mongoose.model('User', userSchema);
 
-const validateUser = (user) => {
-    const schema = Joi.object({
-        name: Joi.string().required(),
-        email: Joi.string().email().required(),
-        password: Joi.string().required(),
-    });
-    return schema.validate(user);
-};
-
-module.exports = { User, validateUser };
+module.exports = { User};
