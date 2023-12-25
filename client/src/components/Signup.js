c08cce615477848eb45870a9dee0ab51ae53f8ab
Gautam Raj <gautamraj996@gmail.com>
2023-12-26 02:15:48 +0530

    google auth done (#263)

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -3,6 +3,9 @@ import { useState } from "react";
 import "../index.css";
 import "../styles/Signup.css";
 import { ReactComponent as Close } from "../icons/close.svg";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { addDoc, getDocs, query, where } from "firebase/firestore";
+import { usersCollection } from "../firebaseConfig";
 
 function Signup ({ setShowSignupForm }) {
   const [invalidEmailFOrmat, setInvalidEmailFormat] = useState(false);
@@ -59,6 +62,35 @@ function Signup ({ setShowSignupForm }) {
     }
   };
 
+  const handleGoogleSignIn = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth();
+      const response = await signInWithPopup(auth, provider);
+      const userObject = {
+        name: response.user.displayName,
+        email: response.user.email,
+        profilePicture: response.user.photoURL
+      };
+      const existingUserQuery = query(usersCollection, where("email", "==", userObject.email));
+      const existingUserSnapshot = await getDocs(existingUserQuery);
+      if (existingUserSnapshot.size === 0) {
+        const addedUser = await addDoc(usersCollection, userObject);
+        console.log("New User added");
+        localStorage.setItem("expenseTrackerUserFirebaseRefId", JSON.stringify(addedUser.id));
+        window.location.reload();
+      } else {
+        existingUserSnapshot.forEach((doc) => {
+          console.log("User already exists: ", doc.id);
+          localStorage.setItem("expenseTrackerUserFirebaseRefId", JSON.stringify(doc.id));
+          window.location.reload();
+        });
+      }
+    } catch (error) {
+      console.log("Some error occurred", error);
+    }
+  };
+
   return (
         <div className="signup_form_container" onClick={(e) => e.stopPropagation()}>
             <h1>Create Your Account</h1>
@@ -107,7 +139,7 @@ function Signup ({ setShowSignupForm }) {
                         )}
                 </button>
                 <div className="signupSeparator flex justify-center items-center" style={{ width: "100%" }}><hr style={{ width: "100%" }}></hr> &nbsp;&nbsp;or&nbsp;&nbsp; <hr style={{ width: "100%" }}></hr></div>
-                <button className="googleSignup p-2 border flex justify-center gap-2 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150" style={{ backgroundColor: "white", color: "black", animationDelay: "1.7s" }}>
+                <button className="googleSignup p-2 border flex justify-center gap-2 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150" style={{ backgroundColor: "white", color: "black", animationDelay: "1.7s" }} onClick={handleGoogleSignIn}>
                     <img className="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo" />
                     <span>Continue with Google</span>
                 </button>
