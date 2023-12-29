2eb9cbba6b394de5951752c34475b903ba2f602a
msho <mayo_s@hotmail.co.uk>
2023-12-29 01:37:14 +0000

    added smooth dark mode transition

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -11,14 +11,19 @@ function Home ({ userJWTToken, userFirebaseRefId, showSignupForm, setShowSignupF
   const { theme } = useContext(ThemeContext);
   return (
         <div>
-                <div className="bg-violet dark:bg-[rgba(19,43,57,1)] text-white flex justify-center"></div>
+                <div className="bg-violet transition duration-500 dark:bg-[rgba(19,43,57,1)] text-white flex justify-center"></div>
             {
-              (theme === "dark")
-                ? <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className="absolute bg ">
-              <path fill="rgba(19,43,57,1)" d="M0,32L120,58.7C240,85,480,139,720,144C960,149,1200,107,1320,85.3L1440,64L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z"></path></svg>
-                : <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className="absolute bg">
-                <path fill="#b6cefc" d="M0,32L120,58.7C240,85,480,139,720,144C960,149,1200,107,1320,85.3L1440,64L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z"></path>
-                </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className="absolute bg">
+              <path
+                className="transition duration-500"
+                fill={
+                  (theme === "dark")
+                    ? "rgba(19,43,57,1)"
+                    : "#c465c9"
+                }
+                d="M0,32L120,58.7C240,85,480,139,720,144C960,149,1200,107,1320,85.3L1440,64L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z"
+              ></path>
+              </svg>
             }
             <Navbar showSignupForm={showSignupForm} setShowSignupForm={setShowSignupForm} />
             {userJWTToken || userFirebaseRefId ? <ExpenseTracker /> : <NoUser showSignupForm={showSignupForm} setShowSignupForm={setShowSignupForm} />}
