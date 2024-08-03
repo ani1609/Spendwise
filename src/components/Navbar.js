8d30ff0015465809a040a80d9a632b676b1fa787
Ankit Kr Chowdhury <rakesh856100@gmail.com>
2024-08-03 15:30:23 +0530

    icon dimension changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -80,7 +80,7 @@ function Navbar(props) {
               stroke-linecap="round"
               stroke-linejoin="round"
               class="lucide lucide-github"
-              className="w-8 h-8 max-md:w-7 max-md:h-7 max-sm:w-6 max-sm:h-6 max-[400px]:w-5 max-[400px]:h-5"
+              className="w-6 h-6 max-sm:w-5 max-sm:h-5"
             >
               <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
               <path d="M9 18c-4.51 2-5-2-7-2" />
@@ -91,24 +91,18 @@ function Navbar(props) {
           <div className="p-2 max-sm:p-1 cursor-pointer" onClick={toggle}>
             {theme === "dark" ? (
               <>
-                <Moon
-                  fill="white"
-                  className="w-8 h-8 max-md:w-7 max-md:h-7 max-sm:w-6 max-sm:h-6 max-[400px]:w-5 max-[400px]:h-5"
-                />
+                <Moon fill="white" className="w-6 h-6 max-sm:w-5 max-sm:h-5" />
               </>
             ) : (
               <>
-                <Sun
-                  fill="white"
-                  className="w-8 h-8 max-md:w-7 max-md:h-7 max-sm:w-6 max-sm:h-6 max-[400px]:w-5 max-[400px]:h-5"
-                />
+                <Sun fill="white" className="w-6 h-6 max-sm:w-5 max-sm:h-5" />
               </>
             )}
           </div>
         )}
         {userFirebaseRefId && (
           <div className="p-2 max-sm:p-1 cursor-pointer" onClick={handleLogOut}>
-            <LogOut className="w-8 h-8 max-md:w-7 max-md:h-7 max-sm:w-6 max-sm:h-6 max-[400px]:w-5 max-[400px]:h-5" />
+            <LogOut className="w-6 h-6 max-sm:w-5 max-sm:h-5" />
           </div>
         )}
         {userFirebaseRefId ? (
