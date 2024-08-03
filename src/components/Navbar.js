e8e3234a0d1b2092501732f12ce66d5ec9ecb1cd
Ankit Kr Chowdhury <rakesh856100@gmail.com>
2024-08-03 15:23:47 +0530

    css tweaks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -58,39 +58,64 @@ function Navbar(props) {
         href="https://spendwise-two.vercel.app/"
         className="flex justify-center gap-2 items-center"
       >
-        <SiMoneygram className="w-9 h-9 max-md:w-8 max-md:h-8 max-sm:w-7 max-sm:h-7" />
+        <SiMoneygram className="w-9 h-9 max-md:w-8 max-md:h-8 max-sm:w-7 max-sm:h-7 max-[400px]:w-6 max-[400px]:h-6" />
         <span>SPENDWISE</span>
       </a>
       <div className="flex gap-x-4 max-sm:gap-x-2 justify-center items-center">
-        {userFirebaseRefId ? (
-          <div className="p-2 cursor-pointer" onClick={toggle}>
+        {userFirebaseRefId && (
+          <a
+            className="p-2 max-sm:p-1 cursor-pointer"
+            href="https://github.com/ani1609/Spendwise"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-github"
+              className="w-8 h-8 max-md:w-7 max-md:h-7 max-sm:w-6 max-sm:h-6 max-[400px]:w-5 max-[400px]:h-5"
+            >
+              <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+              <path d="M9 18c-4.51 2-5-2-7-2" />
+            </svg>
+          </a>
+        )}
+        {userFirebaseRefId && (
+          <div className="p-2 max-sm:p-1 cursor-pointer" onClick={toggle}>
             {theme === "dark" ? (
               <>
                 <Moon
                   fill="white"
-                  className="w-8 h-8 max-md:w-7 max-md:h-7 max-sm:w-6 max-sm:h-6"
+                  className="w-8 h-8 max-md:w-7 max-md:h-7 max-sm:w-6 max-sm:h-6 max-[400px]:w-5 max-[400px]:h-5"
                 />
               </>
             ) : (
               <>
                 <Sun
                   fill="white"
-                  className="w-8 h-8 max-md:w-7 max-md:h-7 max-sm:w-6 max-sm:h-6"
+                  className="w-8 h-8 max-md:w-7 max-md:h-7 max-sm:w-6 max-sm:h-6 max-[400px]:w-5 max-[400px]:h-5"
                 />
               </>
             )}
           </div>
-        ) : null}
+        )}
         {userFirebaseRefId && (
-          <div className="p-2 cursor-pointer" onClick={handleLogOut}>
-            <LogOut className="w-8 h-8 max-md:w-7 max-md:h-7 max-sm:w-6 max-sm:h-6" />
+          <div className="p-2 max-sm:p-1 cursor-pointer" onClick={handleLogOut}>
+            <LogOut className="w-8 h-8 max-md:w-7 max-md:h-7 max-sm:w-6 max-sm:h-6 max-[400px]:w-5 max-[400px]:h-5" />
           </div>
         )}
         {userFirebaseRefId ? (
           <div className="profile_container">
             {/* <Link to="/profile" className="items-center flex flex-col text-xs"> */}
             {/* Link to the /profile route */}
-            <div className="p-2">
+            <div className="p-2 max-sm:p-1">
               <img
                 src={user.profilePicture}
                 alt="profile"
